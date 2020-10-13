d74da3edda7e2c6c1b1e9fc55e116779823a9e76
Adam Gerecke <adamgerecke@quickenloans.com>
2020-10-13 08:55:05 -0400

    Seeing if a stage will change the error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {formsModule} from '@angulat/forms';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostFormComponent } from './post-form/post-form.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
